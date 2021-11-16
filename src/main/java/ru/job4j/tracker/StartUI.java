856e0df139dc248dcce9d18a512b401ad91d875e
alsignat <alsignat@gmail.com>
2021-11-16 20:37:34 +0300

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item anItem = new Item();
+        String timeInfo = anItem.getTimeCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss"));
+        System.out.println(timeInfo);
+    }
+
+}
