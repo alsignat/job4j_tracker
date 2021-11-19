9a20ab7c8c828cbffba8bb52bd7b98f9dc4396d4
alsignat <alsignat@gmail.com>
2021-11-19 19:46:06 +0300

    upd delete method

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -39,7 +39,7 @@ public class Tracker {
             return false;
         }
         System.arraycopy(items, index + 1, items, index, size - index - 1);
-        items[items.length - 1] = null;
+        items[size - 1] = null;
         size--;
         return true;
     }
