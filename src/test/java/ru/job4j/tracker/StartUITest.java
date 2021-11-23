e8e5b4b685ada5228b27d189cf964bf555c6355b
alsignat <alsignat@gmail.com>
2021-11-23 22:14:15 +0300

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -18,4 +18,29 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertEquals(expected.getName(), created.getName());
     }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertEquals(edited.getName(), "edited item");
+    }
+
+    @Test
+    public void deleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        int deletedItemId = tracker.findAll()[0].getId();
+        boolean testBooleanReturn = tracker.delete(deletedItemId);
+        boolean testTrackerUpdated = tracker.findById(deletedItemId) == null;
+        assertTrue(testBooleanReturn && testTrackerUpdated);
+    }
 }
