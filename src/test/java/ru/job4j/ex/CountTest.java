cf6deda3fc2133da55838cb09e9c6516454146f6
alsignat <alsignat@gmail.com>
2021-12-09 21:47:58 +0300

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenStartGreaterThenFinish() {
+        Count.add(10, 2);
+    }
+}
