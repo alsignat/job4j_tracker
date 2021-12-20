0f6d77b42eca74391f3a7ed95585026699cb1a3d
alsignat <alsignat@gmail.com>
2021-12-20 23:24:02 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertEquals(office.get(citizen.getPassport()), citizen);
+    }
+
+    @Test
+    public void addDublicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        boolean rsl = office.add(citizen);
+        assertFalse(rsl);
+    }
+}
